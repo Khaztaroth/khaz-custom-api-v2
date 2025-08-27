3cc552580aab31ae621ac7779436b813bba1b618
khaztaroth315 <astaroth503@gmail.com>
2025-08-27 16:39:42 -0400

    awaiting KV put

--- src/routes/quotes.ts
+++ src/routes/quotes.ts
@@ -22,14 +22,14 @@ export async function GenerateKey(request: Request) {
 
 export async function SaveQuote(request: Request, env: Env): Promise<Response> {
     const parameters = new URL(request.url).searchParams
-    const Channel = parameters.get('channel')
-    const QuoteParam = parameters.get('quote')
-    const KeyParam = parameters.get('key')
-    const CategoryParam = parameters.get('category')
-    const TimezoneParam = parameters.get('timezone') || ''
-    const DateFormatParam = parameters.get('format') || ''
-    const FormattingParam = parameters.get('short') || ''
-    const PersonName = parameters.get('name') || ''
+    const Channel = parameters.get('channel') // Required, channel name
+    const KeyParam = parameters.get('key') // Required, a key provided by the admin that will allow the data to be passed through
+    const QuoteParam = parameters.get('quote') // Required, can't store a quote if you don't have any quotes
+    const CategoryParam = parameters.get('category') // Optional, save which category the user was streaming in when the quote was saved
+    const TimezoneParam = parameters.get('timezone') // Optional, change the timezone the quote is being saved in, useful when days roll over
+    const DateFormatParam = parameters.get('format') // Optional, set what format the date is being written with, day-month-year or month-day-year
+    const FormattingParam = parameters.get('formatted') // Optional, set whether or not the quote will be automatically formatted, if not, it will be passed directly
+    const PersonName = parameters.get('name') // Optional, set a name to use for the "while PERSON streamed...", in case a short name is prefered over the channel name
 
     const UserKeys: Keys = env.QUOTE_KEYS as unknown as Keys
     if (!QuoteParam) {
@@ -61,19 +61,20 @@ export async function SaveQuote(request: Request, env: Env): Promise<Response> {
     }
 
     const ChannelDBName = `${Channel}-quotes`
-    const formatting = () => {if(FormattingParam.toLowerCase() == 'false') {return false} else {return true}}
-    const dateFormat = () => {if(DateFormatParam.toUpperCase() == 'EU') {return 'dd/MM/yy'}else{return 'MM/dd/yy'}}
-    const CurrentDate = DateTime.now().setZone(zoneMatch(TimezoneParam)).toFormat(dateFormat())
+    const formatting = () => {if(FormattingParam && FormattingParam.toLowerCase() == 'false') {return false} else {return true}}
+    const dateFormat = () => {if(DateFormatParam && DateFormatParam.toUpperCase() == 'EU') {return 'dd/MM/yy'}else{return 'MM/dd/yy'}}
+    const CurrentDate = DateTime.now().setZone(zoneMatch(TimezoneParam || '')).toFormat(dateFormat())
 
-    function writetoDB(quote: string, quoteDB?: Record<number, string>) {
+    async function writetoDB(quote: string, quoteDB?: Record<number, string>) {
         if (quoteDB){
                 var quoteCount = Object.keys(quoteDB).length
                 quoteDB[quoteCount+1] = quote
-                env.quotes.put(ChannelDBName, JSON.stringify(quoteDB))
+                await env.quotes.put(ChannelDBName, JSON.stringify(quoteDB))
+                
         } else {
             var placeHolder: Record<number, string> = {1: ''}
             placeHolder[1] = quote
-            env.quotes.put(ChannelDBName, JSON.stringify(placeHolder))
+            await env.quotes.put(ChannelDBName, JSON.stringify(placeHolder))
         }
             }
 
@@ -83,11 +84,11 @@ export async function SaveQuote(request: Request, env: Env): Promise<Response> {
         if (formatting() == true) {
             const Quote = `${QuoteParam}, ${CategoryParam? `while ${PersonName? PersonName : Channel} streamed ${CategoryParam}, ${CurrentDate}`:  `${CurrentDate}`}`
             if (channelQuoteDB){
-                writetoDB(Quote, channelQuoteDB)
+                await writetoDB(Quote, channelQuoteDB)
                 return new Response(`Successfully saved quote #${Object.keys(channelQuoteDB).length}`, {status: 200})
             }
             if (!channelQuoteDB) {
-                writetoDB(Quote)
+                await writetoDB(Quote)
                 return new Response(`Successfully saved quote #${Object.keys(placeHolder).length}`, {status: 200})
             }
         }
@@ -107,12 +108,12 @@ export async function SaveQuote(request: Request, env: Env): Promise<Response> {
 
 export async function DeleteQuote(request: Request, env: Env) {
     const parameters = new URL(request.url).searchParams
-    const Channel = parameters.get('channel')
-    const QueryParam = parameters.get('query')    
-    const KeyParam = parameters.get('key')
+    const Channel = parameters.get('channel') // Required, channel name
+    const KeyParam = parameters.get('key') // Required, a key provided by the admin that will allow the data to be passed through
+    const IndexParam = parameters.get('query') // Required, need to provide index of the quote being deleted
 
     const UserKeys: Keys = env.QUOTE_KEYS as unknown as Keys
-    if (!QueryParam) {
+    if (!IndexParam) {
         return new Response("I need to know what you're looking for, buster.", {status: 400})
     }
     if (!Channel) {
@@ -128,9 +129,9 @@ export async function DeleteQuote(request: Request, env: Env) {
 
     const ChannelDBName = `${Channel}-quotes`
 
-    if(QueryParam) {
+    if(IndexParam) {
         const QuoteList: Record<number, string> | null = await env.quotes.get(ChannelDBName, {type: "json"})
-        const number = +QueryParam
+        const number = +IndexParam
         if (QuoteList && !Number.isNaN(number)) {
             if (QuoteList[number]){
                 delete QuoteList[number]
@@ -156,16 +157,16 @@ export async function DeleteQuote(request: Request, env: Env) {
 
 export async function ModifyQuote(request: Request, env: Env) {
     const parameters = new URL(request.url).searchParams
-    const Channel = parameters.get('channel')
-    const QuoteParam = parameters.get('quote')
-    const IndexParam = parameters.get('index') 
-    const KeyParam = parameters.get('key')
+    const ChannelParam = parameters.get('channel') // Required, channel name
+    const KeyParam = parameters.get('key') // Required, a key provided by the admin that will allow the data to be passed through
+    const QuoteParam = parameters.get('quote') // Required, need to have a quote to replace the old one
+    const IndexParam = parameters.get('index') // Required, have to know which quote index is being replaced, useful when replacing quotes much after
 
     const UserKeys: Keys = env.QUOTE_KEYS as unknown as Keys
     if (!QuoteParam) {
         return new Response("I need to know what you're looking for, buster.", {status: 400})
     }
-    if (!Channel) {
+    if (!ChannelParam) {
         return new Response("Whose channel is it tho?", {status: 400})
     }
     if(!IndexParam) {
@@ -174,12 +175,12 @@ export async function ModifyQuote(request: Request, env: Env) {
     if(!KeyParam){
         return new Response('Key not found', {status: 401})
     }
-    if (+KeyParam && UserKeys[Channel as keyof Keys] !== KeyParam) {
+    if (+KeyParam && UserKeys[ChannelParam as keyof Keys] !== KeyParam) {
         console.error(KeyParam, UserKeys.Khaztaroth315)
         return new Response("Invalid key.", {status: 401})
     }
 
-    const ChannelDBName = `${Channel}-quotes`
+    const ChannelDBName = `${ChannelParam}-quotes`
 
     if(IndexParam && QuoteParam) {
         const number = +IndexParam
@@ -198,10 +199,10 @@ export async function ModifyQuote(request: Request, env: Env) {
 
 export async function InsertQuote(request: Request, env: Env) {
     const parameters = new URL(request.url).searchParams
-    const Channel = parameters.get('channel')
-    const QuoteParam = parameters.get('quote')
-    const IndexParam = parameters.get('index') 
-    const KeyParam = parameters.get('key')
+    const Channel = parameters.get('channel') // Required, channel name
+    const KeyParam = parameters.get('key') // Required, a key provided by the admin that will allow the data to be passed through
+    const QuoteParam = parameters.get('quote') // Required, need to provide the text for the new quote
+    const IndexParam = parameters.get('index') // Required, have to have an index to insert the quote in, so all the rest can be moved accordingly
 
     const UserKeys: Keys = env.QUOTE_KEYS as unknown as Keys
     if (!QuoteParam) {
@@ -250,9 +251,9 @@ export async function InsertQuote(request: Request, env: Env) {
 
 export async function FindQuote(request: Request, env: Env) {
     const parameters = new URL(request.url).searchParams
-    const Channel = parameters.get('channel')
-    const QuerryParam = parameters.get('query')    
-    const KeyParam = parameters.get('key')
+    const Channel = parameters.get('channel') // Required, channel name
+    const KeyParam = parameters.get('key') // Required, a key provided by the admin that will allow the data to be passed through
+    const QuerryParam = parameters.get('query') // Optional, if present it will check for either a number or a string and find the corresponding quote, if empty it will give a random quote
 
     const UserKeys: Keys = env.QUOTE_KEYS as unknown as Keys
     if (!Channel) {
